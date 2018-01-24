04c44d6651c2da7e845dfcf0eaf15edc700b513d
Jonathan Pool <pool@stulta.com>
2018-01-24 14:50:53 -0800

    Made images and hyperlinks appear as such.

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
+const isLink = string => {
+  return string.startsWith('http://') || string.startsWith('https://');
+};
+
+const isImageLink = string => {
+  return isLink(string) && /\.(?:jpg|jpeg|gif|png|svg)$/.test(string);
+};
+
+const linkify = string => {
+  return `<a href="${string}">${string}</a>`;
+};
+
+const imageLinkify = (string, alt) => {
+  return `<img src="${string}" alt="${alt}">`;
+};
+
 const renderHead = (string, hLevel) => {
   const tagName = `h${hLevel}`;
   const openTag = `<${tagName}>`;
@@ -12,18 +28,27 @@ const renderString = string => {
   return `<p>${string}</p>\n`;
 };
 
-const renderItem = (item, hLevel) => {
+const renderItem = (propertyName, item, hLevel) => {
   const itemType = typeof item;
   if (Array.isArray(item)) {
-    return item.map(element => renderItem(element, hLevel)).join('');
+    return item.map(element => renderItem(
+      propertyName, element, hLevel)
+    ).join('');
   }
   else if (['number', 'string', 'boolean'].includes(itemType)) {
+    item = item.toString();
+    if (isImageLink(item)) {
+      item = imageLinkify(item, propertyName);
+    }
+    else if (isLink(item)) {
+      item = linkify(item);
+    }
     return renderString(item);
   }
   else if (itemType === 'object') {
-    return Object.keys(item).map(propertyName => [
-      renderHead(propertyName, hLevel),
-      renderItem(item[propertyName], hLevel + 1)
+    return Object.keys(item).map(subPropertyName => [
+      renderHead(subPropertyName, hLevel),
+      renderItem(subPropertyName, item[subPropertyName], hLevel + 1)
     ].join('')).join('');
   }
   else {
@@ -35,7 +60,7 @@ const render = cvObject => {
   const css = fs.readFileSync(
     path.join(__dirname, 'style.css'), 'utf-8'
   ).slice(0, -1).split('\n').join('\n      ');
-  const html = renderItem(cvObject, 1).split('\n').join('\n      ');
+  const html = renderItem('', cvObject, 1).split('\n').join('\n      ');
   const template = fs.readFileSync(
     path.join(__dirname, 'template.html'), 'utf-8'
   );
